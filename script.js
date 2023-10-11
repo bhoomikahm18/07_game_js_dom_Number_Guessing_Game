f6138852ad5c48f36b160cd869defa55024cec9f
Bhoomika <bhoomikahm18@gmail.com>
2023-10-11 15:37:24 +0530

    Success alert for currect guess

--- script.js
+++ script.js
@@ -30,8 +30,25 @@ const play = () => {
         setTimeout(() => {
             hint.classList.add("error");
         }, 10);
+    } else {
+        hint.innerHTML = `Congratulation!<br>The number was<span>${answer}</span>.<br>You guessed the number in<span>${noOfGuesses}</span> tries`;
+        hint.classList.add("success");
+        game.style.display = 'none';
+        restartButton.style.display = 'block';
     }
 };
 
+const init = () => {
+    console.log("Game Started");
+    answer = Math.floor(Ma.random() * 100 + 1);
+    console.log(answer);
+    noOfGuesses = 0;
+    guessedNumsArr = [];
+    noOfGuessRef.innerHTML = "Guessed Number are: None";
+    guessInput.value = "";
+    hint.classList.remove("success", "error");
+}
+
+window.addEventListener("Load", init);
 
 checkButton.addEventListener("click", play)
