e80c8fc6afd037b9c72eb171ac192909b4f1b460
Bhoomika <bhoomikahm18@gmail.com>
2023-10-11 15:32:19 +0530

    If statement for incurrect number

--- script.js
+++ script.js
@@ -8,15 +8,30 @@ const checkButton = document.getElementById("check-btn");
 
 let answer, noOfGuesses, guessedNumsArr;
 
-const play = () =>{
+const play = () => {
     const userGuess = guessInput.ariaValueMax;
-    if(userGuess < 1 || userGuess > 100 || isNaN(userGuess)){
+    if (userGuess < 1 || userGuess > 100 || isNaN(userGuess)) {
         alert("Please enter a valid number between 1 to 100.")
         return;
     }
     guessedNumsArr.push(userGuess);
     noOfGuesses += 1;
-    
-}
+    if (userGuess != answer) {
+        if (userGuess < answer) {
+            hint.innerHTML = "Too low. Try Again!";
+        } else {
+            hint.innerHTML = "Too high. Try Again!";
+        }
+        noOfGuessRef.innerHTML = `<span>No. Of Guesses: </span> ${noOfGuesses}`;
+        guessedNumsRef.innerHTML = `<span>Guessed Numbers are: </span> ${guessedNumsArr.join(
+            ","
+        )}`;
+        hint.classList.remove("error");
+        setTimeout(() => {
+            hint.classList.add("error");
+        }, 10);
+    }
+};
+
 
 checkButton.addEventListener("click", play)
